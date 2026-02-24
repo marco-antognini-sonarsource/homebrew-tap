6e2529e4671c30d1c1e5c2ef3b12d8b938bb08a4
Marco Borgeaud <marco.borgeaud@sonarsource.com>
2026-02-24 18:45:48 +0100

    Update claude-code@stable to version 2.1.44

--- Casks/claude-code@stable.rb
+++ Casks/claude-code@stable.rb
@@ -2,11 +2,11 @@ cask "claude-code@stable" do
   arch arm: "arm64", intel: "x64"
   os macos: "darwin", linux: "linux"
 
-  version "2.1.39"
-  sha256 arm:          "d8d7a2b996a691036a53933a259a532254a400934aee452e289e1f4443026d82",
-         x86_64:       "2cba24a410a5226f9750b8fe7e83dbe9a61485ef9b0eb286b0b00438aa990d05",
-         x86_64_linux: "68e4775b293d95e06d168581c523fc5c1523968179229d31a029f285b2aceaff",
-         arm64_linux:  "8f66e02a5be8a620e286f6e634fb424b5ac065731b048af3d745cf719b2c7851"
+  version "2.1.44"
+  sha256 arm:          "03cb80fe89e645f465a11b519fc31bd4295c30d33f47bd84be5a018fffd7a4c7",
+         x86_64:       "ff75130b5091baeb85c2e4e1183d778b3c44079f3638f90f7bcd2a267d2181a5",
+         x86_64_linux: "090ed3f06be51388bc511f90214484891432c160349c9c3fc6a131001c763381",
+         arm64_linux:  "335f440cfa4c3b020e79896a00bfb68376c6efb11051c7e6736732acdc74d6ee"
 
   url "https://storage.googleapis.com/claude-code-dist-86c565f3-f756-42ad-8dfa-d59b1c096819/claude-code-releases/#{version}/#{os}-#{arch}/claude",
       verified: "storage.googleapis.com/claude-code-dist-86c565f3-f756-42ad-8dfa-d59b1c096819/claude-code-releases/"
