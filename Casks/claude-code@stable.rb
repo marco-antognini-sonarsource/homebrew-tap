e7d27fb4a0a896bf23cf346d0b6e60922c35da2d
Marco Borgeaud <marco.borgeaud@sonarsource.com>
2026-03-30 11:03:57 +0200

    Revert "Update claude-code@stable to version 2.1.81"
    This reverts commit 3527b5e990a3a89e90eb88395473d7d7518465ca.

--- Casks/claude-code@stable.rb
+++ Casks/claude-code@stable.rb
@@ -2,11 +2,11 @@ cask "claude-code@stable" do
   arch arm: "arm64", intel: "x64"
   os macos: "darwin", linux: "linux"
 
-  version "2.1.81"
-  sha256 arm:          "d014162177fc18bfeb7f93d942130dd964f7424e4101f6ad569de66e6eddca03",
-         x86_64:       "3c63d8173d4a86ab3985aead73c4699e42956d10c2f946179274be76ec657099",
-         x86_64_linux: "047e3f5591d6238b08dd9518729ac335b0e8df1c80fe985e5d7fbda2c18fc281",
-         arm64_linux:  "ccfc3845c8d1a2ded9656a3a517694a844a1b7005b87c784a66f7a60cc58012c"
+  version "2.1.58"
+  sha256 arm:          "182c20c6080d042e4e08a6b2a2ce8258c1a50e53c01d36ddf20a82b4693395ea",
+         x86_64:       "fb2ccc6872d498fc571f302059d51aec7c89b0df4d21d247a9a249c1a3a835dc",
+         x86_64_linux: "10f68e45b5ff4a80ae21e6d7a95dfe673ab3391313358ccd94e827f379f6aaa0",
+         arm64_linux:  "5edd4b6be6b9e9c00e5805f022ee924a98fdcf1bf0c04635a429d2666eccbc3f"
 
   url "https://storage.googleapis.com/claude-code-dist-86c565f3-f756-42ad-8dfa-d59b1c096819/claude-code-releases/#{version}/#{os}-#{arch}/claude",
       verified: "storage.googleapis.com/claude-code-dist-86c565f3-f756-42ad-8dfa-d59b1c096819/claude-code-releases/"
